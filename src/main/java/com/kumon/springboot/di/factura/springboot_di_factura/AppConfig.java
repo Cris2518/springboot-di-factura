f2463f73dc2665e3da0ba65a90e7305945e8fea8
Cristian Ortega <cristian123.cuop@gmail.com>
2025-07-31 01:26:02 -0600

    Inyeccion de un Bean en especifico

--- src/main/java/com/kumon/springboot/di/factura/springboot_di_factura/AppConfig.java
+++ src/main/java/com/kumon/springboot/di/factura/springboot_di_factura/AppConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 
 import com.kumon.springboot.di.factura.springboot_di_factura.models.Item;
@@ -14,7 +15,7 @@ import com.kumon.springboot.di.factura.springboot_di_factura.models.Product;
 @PropertySource(value = "classpath:data.properties", encoding = "UTF-8")
 public class AppConfig {
 
-    @Bean
+    @Bean("default")
     List<Item> itemsInvoice() {
 
         Product p1 = new Product("Camara", 800);
@@ -24,6 +25,25 @@ public class AppConfig {
         List<Item> listItems = Arrays.asList(new Item(p1, 2),
                 new Item(p2, 4),
                 new Item(p3, 2));
+
+        return listItems;
+
+    }
+
+    @Bean
+    List<Item> itemsInvoiceOficiona() {
+
+        Product p1 = new Product("Monitor", 800);
+        Product p2 = new Product("Teclado", 60);
+        Product p3 = new Product("IPhone", 500);
+        Product p4 = new Product("MacBook", 1200);
+        Product p5 = new Product("Hojas", 190);
+
+        List<Item> listItems = Arrays.asList(new Item(p1, 2),
+                new Item(p2, 4),
+                new Item(p3, 2),
+                new Item(p4, 1),
+                new Item(p5, 10));
 
         return listItems;
 
